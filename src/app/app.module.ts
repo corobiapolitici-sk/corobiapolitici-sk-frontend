3a3e2f9ac63dea54646d320146365ad46fae7faf
filek <martin.fillek@gmail.com>
2018-09-10 18:18:14 +0200

    added home-section, home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './atoms/button/button.component';
+import { AppBarComponent } from './atoms/app-bar/app-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HomeSectionComponent } from './templates/home-section/home-section.component';
+import { CardComponent } from './atoms/card/card.component';
+import { CardWithIconComponent } from './molecules/card-with-icon/card-with-icon.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    AppBarComponent,
+    HomeComponent,
+    HomeSectionComponent,
+    CardComponent,
+    CardWithIconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
