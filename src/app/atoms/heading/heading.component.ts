4a7fcc1ed698d2284ba3e7a31f2d0299c7c6180e
Martin Filek <martin.fillek@gmail.com>
2018-09-11 00:00:09 +0200

    added component for heading

---
+++ src/app/atoms/heading/heading.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.scss']
+})
+export class HeadingComponent {
+  @Input() text: string;
+  @Input() type ? = 'h1';
+}
