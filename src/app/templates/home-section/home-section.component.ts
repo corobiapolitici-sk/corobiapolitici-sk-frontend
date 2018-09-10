f9cee5602f9eeb047eaf6ba847ff858d7cb92aed
Martin Filek <martin.fillek@gmail.com>
2018-09-11 00:03:16 +0200

    added all components to home-section

--- src/app/templates/home-section/home-section.component.ts
+++ src/app/templates/home-section/home-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-section',
   templateUrl: './home-section.component.html',
-  styleUrls: ['./home-section.component.css']
+  styleUrls: ['./home-section.component.scss']
 })
 export class HomeSectionComponent implements OnInit {
 
