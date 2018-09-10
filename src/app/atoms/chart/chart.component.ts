0012c85d5c191f8ed00606c949e49a9bd66ddc21
Martin Filek <martin.fillek@gmail.com>
2018-09-10 23:59:47 +0200

    added component for chart

---
+++ src/app/atoms/chart/chart.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
+})
+export class ChartComponent {
+  options = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+      }
+    ]
+  };
+}
